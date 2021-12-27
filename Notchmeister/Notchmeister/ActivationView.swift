3c1dc64b952a21d1cbd5593019a4b38a0a1ddedd
Craig Hockenberry <craig.hockenberry@gmail.com>
2021-12-27 15:42:20 -0800

    The control panel Easter egg will only be shown with a real notch.

--- Notchmeister/Notchmeister/ActivationView.swift
+++ Notchmeister/Notchmeister/ActivationView.swift
@@ -18,40 +18,12 @@ class ActivationView: NSView {
 		}
 		dirtyRect.fill()
 
-		if let image = NSImage(named: "controlpanel") {
-			let drawRect = CGRect(origin: CGPoint(x: bounds.midX - image.size.width / 2, y: bounds.minY + NotchWindow.activationPadding + 1), size: image.size)
-			//let drawRect = CGRect(origin: CGPoint(x: bounds.midX - image.size.width / 2, y: bounds.maxY - image.size.height), size: image.size)
-			image.draw(in: drawRect, from: .zero, operation: .sourceOver, fraction: 1)
-		}
-	}
-
-/*
-	//MARK: - NSResponder
-
-	override func acceptsFirstMouse(for event: NSEvent?) -> Bool {
-		return false
-	}
-	
-	override func mouseDown(with event: NSEvent) {
-#if false
-		let padding = NotchWindow.padding
-		let notchRect = CGRect(x: bounds.origin.x + padding, y: bounds.origin.y + padding, width: bounds.width - (padding * 2), height: bounds.height - padding)
-		
-		let location = event.locationInWindow
-		debugLog("location = \(location)")
-		if notchRect.contains(location) {
-			if let window = NSApplication.shared.windows.first {
-				window.makeKeyAndOrderFront(self)
+		if NSScreen.hasNotchedScreen {
+			if let image = NSImage(named: "controlpanel") {
+				let drawRect = CGRect(origin: CGPoint(x: bounds.midX - image.size.width / 2, y: bounds.minY + NotchWindow.activationPadding + 1), size: image.size)
+				image.draw(in: drawRect, from: .zero, operation: .sourceOver, fraction: 1)
 			}
 		}
-#else
-		let location = event.locationInWindow
-		debugLog("location = \(location)")
-		if let window = NSApplication.shared.windows.first {
-			window.makeKeyAndOrderFront(self)
-		}
-#endif
 	}
- */
 	
 }
