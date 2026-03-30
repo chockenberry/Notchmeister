2a47855fbb8b900d2f2fad24088081264babfa17
Craig Hockenberry <chockenberry@users.noreply.github.com>
2026-03-30 13:23:36 -0700

    Started working on assets and code to draw M + number.

--- Notchmeister/Notchmeister/ActivationView.swift
+++ Notchmeister/Notchmeister/ActivationView.swift
@@ -28,8 +28,14 @@ class ActivationView: NSView {
 			if notchWidth > smallMinimumWidth {
 				// NOTE: The machdep.cpu.brand_string sysctl is used to check for "Apple M2" or "Apple M1"
 				// https://cpufun.substack.com/p/more-m1-fun-hardware-information
+#if DEBUG && true
+				let cpuBrand = "Apple M2"
+#else
 				let cpuBrand = cpuBrand()
+#endif
+				let brandOffset: CGPoint
 				let imageName: String
+#if false
 				if cpuBrand.hasPrefix("Apple M3") {
 					imageName = notchWidth < largeMinimumWidth ? "m3-controlpanel-small" : "m3-controlpanel"
 				}
@@ -39,9 +45,85 @@ class ActivationView: NSView {
 				else {
 					imageName = notchWidth < largeMinimumWidth ? "m1-controlpanel-small" : "m1-controlpanel"
 				}
+#else
+#if DEBUG && true
+				brandOffset = CGPoint(x: 40, y: 14)
+				imageName = "controlpanel-small"
+#else
+				imageName = notchWidth < largeMinimumWidth ? "controlpanel-small" : "controlpanel"
+#endif
+				var totalWidth: CGFloat = 0
+				
+				let prefix = "Apple M"
+				if cpuBrand.hasPrefix(prefix) {
+					//let iteration = cpuBrand.substring(from: prefix.endIndex)
+					let iteration = cpuBrand[prefix.endIndex..<cpuBrand.endIndex]
+					debugLog("iteration = \(iteration)")
+					if let mImage = NSImage(named: "M") {
+						debugLog("mImage.size = \(mImage.size)")
+						totalWidth += mImage.size.width
+					}
+					for number in iteration {
+						debugLog("number = \(number)")
+						if let numberImage = NSImage(named: String(number)) {
+							debugLog("numberImage.size = \(numberImage.size)")
+							totalWidth += numberImage.size.width + 0.5
+						}
+					}
+				}
+				debugLog("totalWidth = \(totalWidth)")
+
+#endif
 				if let image = NSImage(named: imageName) {
 					let drawRect = CGRect(origin: CGPoint(x: bounds.midX - image.size.width / 2, y: bounds.minY + NotchWindow.activationPadding + 1), size: image.size)
 					image.draw(in: drawRect, from: .zero, operation: .sourceOver, fraction: 1)
+										
+					if let mImage = NSImage(named: "M") {
+						let origin = CGPoint(
+							x: ceil(drawRect.origin.x + brandOffset.x - (totalWidth / 2.0)), // - (mImage.size.width / 2.0)),
+							y: ceil(drawRect.origin.y + brandOffset.y - (mImage.size.height / 2.0)))
+						let size = mImage.size
+						let mRect = CGRect(origin: origin, size: size)
+						mImage.draw(in: mRect, from: .zero, operation: .sourceOver, fraction: 1)
+					}
+
+					let iteration = cpuBrand[prefix.endIndex..<cpuBrand.endIndex]
+					for number in iteration {
+						debugLog("number = \(number)")
+						if let numberImage = NSImage(named: String(number)) {
+							let origin = CGPoint(
+								x: floor(drawRect.origin.x + brandOffset.x - (totalWidth / 2.0) - (numberImage.size.width / 2.0)),
+								y: floor(drawRect.origin.y + brandOffset.y - (numberImage.size.height / 2.0)))
+							let size = numberImage.size
+							let numberRect = CGRect(origin: origin, size: size)
+							//numberImage.draw(in: numberRect, from: .zero, operation: .sourceOver, fraction: 1)
+						}
+					}
+
+#if DEBUG && true
+					do {
+						let origin = CGPoint(
+							x: ceil(drawRect.origin.x + brandOffset.x - (totalWidth / 2.0)), // - (mImage.size.width / 2.0)),
+							y: ceil(drawRect.origin.y + brandOffset.y))
+						let size = CGSize(width: totalWidth, height: 0.5)
+						let fillRect = CGRect(origin: origin, size: size)
+
+						NSColor.systemGreen.setFill()
+						fillRect.fill()
+					}
+
+					do {
+						let origin = CGPoint(
+							x: floor(drawRect.origin.x + brandOffset.x), // - (mImage.size.width / 2.0)),
+							y: floor(drawRect.origin.y + brandOffset.y))
+						let size = CGSize(width: 0.5, height: 0.5)
+						let fillRect = CGRect(origin: origin, size: size)
+
+						NSColor.systemRed.setFill()
+						fillRect.fill()
+					}
+#endif
+
 				}
 			}
 		}
