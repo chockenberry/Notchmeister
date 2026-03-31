90c0452e06ef6204801444521e5f3670e4c870b4
Craig Hockenberry <chockenberry@users.noreply.github.com>
2026-03-31 11:46:42 -0700

    FInished support for any M + number combination.

--- Notchmeister/Notchmeister/ActivationView.swift
+++ Notchmeister/Notchmeister/ActivationView.swift
@@ -25,86 +25,86 @@ class ActivationView: NSView {
 			let notchWidth = bounds.width - (NotchWindow.activationPadding * 2)
 			let smallMinimumWidth: CGFloat = 137 + (.notchLowerRadius * 2)
 			let largeMinimumWidth: CGFloat = 192 + (.notchLowerRadius * 2)
+			
 			if notchWidth > smallMinimumWidth {
-				// NOTE: The machdep.cpu.brand_string sysctl is used to check for "Apple M2" or "Apple M1"
-				// https://cpufun.substack.com/p/more-m1-fun-hardware-information
-#if DEBUG && true
-				let cpuBrand = "Apple M2"
+#if DEBUG && false
+				let cpuBrand = "Apple M4"
 #else
+				// NOTE: The machdep.cpu.brand_string sysctl is used to check for "Apple M1", "Apple M2", etc.
+				// https://cpufun.substack.com/p/more-m1-fun-hardware-information
 				let cpuBrand = cpuBrand()
 #endif
+				
 				let brandOffset: CGPoint
 				let imageName: String
-#if false
-				if cpuBrand.hasPrefix("Apple M3") {
-					imageName = notchWidth < largeMinimumWidth ? "m3-controlpanel-small" : "m3-controlpanel"
-				}
-				else if cpuBrand.hasPrefix("Apple M2") {
-					imageName = notchWidth < largeMinimumWidth ? "m2-controlpanel-small" : "m2-controlpanel"
+
+#if DEBUG && false
+				let smallWidth = false
+#else
+				let smallWidth = notchWidth < largeMinimumWidth
+#endif
+				if smallWidth {
+					brandOffset = CGPoint(x: 40.5, y: 14)
+					imageName = "controlpanel-small"
 				}
 				else {
-					imageName = notchWidth < largeMinimumWidth ? "m1-controlpanel-small" : "m1-controlpanel"
+					brandOffset = CGPoint(x: 95.5, y: 14)
+					imageName = "controlpanel"
 				}
-#else
-#if DEBUG && true
-				brandOffset = CGPoint(x: 40, y: 14)
-				imageName = "controlpanel-small"
-#else
-				imageName = notchWidth < largeMinimumWidth ? "controlpanel-small" : "controlpanel"
-#endif
+
 				var totalWidth: CGFloat = 0
+				let drawPadding: CGFloat = 1.0
 				
 				let prefix = "Apple M"
 				if cpuBrand.hasPrefix(prefix) {
-					//let iteration = cpuBrand.substring(from: prefix.endIndex)
-					let iteration = cpuBrand[prefix.endIndex..<cpuBrand.endIndex]
-					debugLog("iteration = \(iteration)")
 					if let mImage = NSImage(named: "M") {
-						debugLog("mImage.size = \(mImage.size)")
 						totalWidth += mImage.size.width
 					}
-					for number in iteration {
-						debugLog("number = \(number)")
+					let numbers = cpuBrand[prefix.endIndex..<cpuBrand.endIndex]
+					for number in numbers {
 						if let numberImage = NSImage(named: String(number)) {
-							debugLog("numberImage.size = \(numberImage.size)")
-							totalWidth += numberImage.size.width + 0.5
+							totalWidth += numberImage.size.width + drawPadding
 						}
 					}
 				}
-				debugLog("totalWidth = \(totalWidth)")
 
-#endif
 				if let image = NSImage(named: imageName) {
 					let drawRect = CGRect(origin: CGPoint(x: bounds.midX - image.size.width / 2, y: bounds.minY + NotchWindow.activationPadding + 1), size: image.size)
 					image.draw(in: drawRect, from: .zero, operation: .sourceOver, fraction: 1)
 										
+					var drawIndent = -totalWidth / 2.0
+					
 					if let mImage = NSImage(named: "M") {
 						let origin = CGPoint(
-							x: ceil(drawRect.origin.x + brandOffset.x - (totalWidth / 2.0)), // - (mImage.size.width / 2.0)),
-							y: ceil(drawRect.origin.y + brandOffset.y - (mImage.size.height / 2.0)))
+							x: drawRect.origin.x + brandOffset.x + pixelClamp(drawIndent), // - (mImage.size.width / 2.0)),
+							y: drawRect.origin.y + brandOffset.y - pixelClamp(mImage.size.height / 2.0))
 						let size = mImage.size
 						let mRect = CGRect(origin: origin, size: size)
 						mImage.draw(in: mRect, from: .zero, operation: .sourceOver, fraction: 1)
+						
+						drawIndent += mImage.size.width + drawPadding
 					}
 
 					let iteration = cpuBrand[prefix.endIndex..<cpuBrand.endIndex]
 					for number in iteration {
-						debugLog("number = \(number)")
 						if let numberImage = NSImage(named: String(number)) {
 							let origin = CGPoint(
-								x: floor(drawRect.origin.x + brandOffset.x - (totalWidth / 2.0) - (numberImage.size.width / 2.0)),
-								y: floor(drawRect.origin.y + brandOffset.y - (numberImage.size.height / 2.0)))
+								x: drawRect.origin.x + brandOffset.x + pixelClamp(drawIndent),
+								y: drawRect.origin.y + brandOffset.y - pixelClamp(numberImage.size.height / 2.0))
 							let size = numberImage.size
 							let numberRect = CGRect(origin: origin, size: size)
-							//numberImage.draw(in: numberRect, from: .zero, operation: .sourceOver, fraction: 1)
+							numberImage.draw(in: numberRect, from: .zero, operation: .sourceOver, fraction: 1)
+
+							// NOTE: "1" is the narrowest image at 3×9 pixels, "4" is the widest at 7×9 pixels, others are 6×9 pixels.
+							drawIndent += numberImage.size.width + drawPadding
 						}
 					}
 
-#if DEBUG && true
+#if DEBUG && false
 					do {
 						let origin = CGPoint(
-							x: ceil(drawRect.origin.x + brandOffset.x - (totalWidth / 2.0)), // - (mImage.size.width / 2.0)),
-							y: ceil(drawRect.origin.y + brandOffset.y))
+							x: drawRect.origin.x + brandOffset.x + pixelClamp(-totalWidth / 2.0),
+							y: drawRect.origin.y + brandOffset.y)
 						let size = CGSize(width: totalWidth, height: 0.5)
 						let fillRect = CGRect(origin: origin, size: size)
 
@@ -114,8 +114,8 @@ class ActivationView: NSView {
 
 					do {
 						let origin = CGPoint(
-							x: floor(drawRect.origin.x + brandOffset.x), // - (mImage.size.width / 2.0)),
-							y: floor(drawRect.origin.y + brandOffset.y))
+							x: drawRect.origin.x + brandOffset.x,
+							y: drawRect.origin.y + brandOffset.y)
 						let size = CGSize(width: 0.5, height: 0.5)
 						let fillRect = CGRect(origin: origin, size: size)
 
@@ -123,10 +123,16 @@ class ActivationView: NSView {
 						fillRect.fill()
 					}
 #endif
-
 				}
 			}
 		}
+	}
+	
+	private func pixelClamp(_ dimension: CGFloat) -> CGFloat {
+		// NOTE: A 2x Retina Display is assumed. Every Mac with a notch has one.
+		let pixelScale: CGFloat = window?.screen?.backingScaleFactor ?? 2.0
+		let result = floor(dimension * pixelScale) / pixelScale
+		return result
 	}
 	
 	private func cpuBrand() -> String {
